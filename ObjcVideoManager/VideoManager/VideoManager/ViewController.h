e036a85c7a564177e2794d88853b61a6081418ca
MD Abul Kashem <kashem@MacBook-Pro.local>
2020-12-26 02:08:46 +0600

    [Title] Add library permission
    Signed-off-by: MD Abul Kashem <kashem@MacBook-Pro.local>

--- ObjcVideoManager/VideoManager/VideoManager/ViewController.h
+++ ObjcVideoManager/VideoManager/VideoManager/ViewController.h
@@ -9,6 +9,8 @@
 
 @interface ViewController : UIViewController
 
+@property (weak, nonatomic) IBOutlet UIButton *choseVideo;
+- (IBAction)loadVideoFromDeviceLibrary:(id)sender;
 
 @end
 
--- ObjcVideoManager/VideoManager/VideoManager/ViewController.m
+++ ObjcVideoManager/VideoManager/VideoManager/ViewController.m
@@ -6,6 +6,8 @@
 //
 
 #import "ViewController.h"
+#import <AVFoundation/AVFoundation.h>
+#import <Photos/Photos.h>
 
 @interface ViewController ()
 
@@ -18,5 +20,63 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)loadVideoFromDeviceLibrary:(id)sender {
+    //Check permission
+    if ([self checkCameraAuthorization] == YES){
+        //Load view controller
+    } else {
+        //Do nothing
+    }
+    //Load view controller
+}
+
+-(BOOL) checkCameraAuthorization {
+    PHAuthorizationStatus status = [PHPhotoLibrary authorizationStatus];
+    switch (status) {
+        case PHAuthorizationStatusAuthorized:
+            //Alredy have the access
+            break;
+        case PHAuthorizationStatusDenied:
+            [self showingPopUpForWaning];
+            return NO;
+        case PHAuthorizationStatusNotDetermined:
+            // Access has not been determined.
+            return [self showingPopUpForLibraryAccess];
+        default:
+            return NO;
+            // Restricted access - normally won't happen.
+    }
+    return NO;
+    
+}
+
+-(BOOL)showingPopUpForLibraryAccess {
+    __block BOOL stat = NO;
+    [PHPhotoLibrary requestAuthorization:^(PHAuthorizationStatus status) {
+        
+        if (status == PHAuthorizationStatusAuthorized) {
+            // Access has been granted.
+            stat = YES;
+        }
+        
+        else {
+            [self showingPopUpForWaning];
+            // Access has been denied.
+        }
+    }];
+    return stat;
+}
+
+-(void)showingPopUpForWaning {
+    UIAlertController* alert = [UIAlertController alertControllerWithTitle:@"Warning"
+                                   message:@"Please go to setting and change app permission"
+                                   preferredStyle:UIAlertControllerStyleAlert];
+
+    UIAlertAction* defaultAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault
+       handler:^(UIAlertAction * action) {}];
+
+    [alert addAction:defaultAction];
+    [self presentViewController:alert animated:YES completion:nil];
+}
 
 @end
